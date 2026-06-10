3253ca1840c512c682a71aee42f972c94a082fda
renzo <renzoarreaza@gmail.com>
2026-06-10 18:06:29 +0200

    added (failing) testcases

--- testdata/main.go
+++ testdata/main.go
@@ -37,17 +37,40 @@ func multipleImplementations() (example2, example2) {
 	return example2{}, example2{}
 }
 
+func errorChecker(err error) {
+	if err != nil {
+		panic("fail")
+	}
+}
+
 // someFunc creates a Closer object but doesn't call close nor returns it, so it should be reported by the analyzer
 func someFunc() {
 	e := example2{} // want "Closed is not called on e"
 	_ = e
 }
 
-// someFunc2 creates a Closer object but doesn't call close but does return it
+// someFunc2 creates a Closer object and returns it instead of closing it
 func someFunc2() example2 {
 	e := example2{} // ok. is returned
 	return e
 }
+
+type structWithCloser struct {
+	field1 *example
+}
+
+/* false-positives
+func returnCloserInStruct() structWithCloser {
+	a := &example{} // false positive: we are returning it in the struct so we shouldn't close it here
+	b := structWithCloser{field1: a}
+	return b
+}
+
+func returnCloserInStruct2() structWithCloser {
+	a := &example{} // false positive: we are returning it in the struct so we shouldn't close it here
+	return structWithCloser{field1: a}
+}
+*/
 
 func main() {
 	// this does not implement io.Closer. Method has pointer receiver
@@ -116,18 +139,27 @@ func main() {
 	r, s := multipleImplementations() // want "Closed is not called on r" "Closed is not called on s"
 	_, _ = r, s
 
-	// TODO
-	// defer func() { assert.NoError(t, h.Close(), "failed to close netns") }()
+	var t example2
+	defer func() { errorChecker(t.Close()) }()
+
+	// TODO: causes panic:
+	// var u any = "asdf"
+	// switch a := u.(type) {
+	// case string:
+	// 	_ = a
+	// default:
+	// }
+
+	// FIXME: causes panic
+	// v := structWithCloser{field1: &example{}}
+	// v.field1.Close() // multiple SelectorExpr
 
 	// TODO: this should report some error since the method does return an implementation of io.Closer, but because I don't assign it to a variable we can't close it
 	// Or, we should treat assignment to _ as an ignore? similar to errorcheck?
 	// _ = getImpl2Ptr() // "Closed is not called on the result of getImpl2Ptr()"
 
-	/* TODO
-	   var a = os.Open("file.txt")
-	   // assuming a is a pointer
-	   b := a
-	   b.Close()
-	   // we shouldn't fail on a not being closed
-	*/
+	// TODO
+	// aa, _ := os.Open("file.txt")
+	// bb := aa   // assuming aa is a pointer
+	// bb.Close() // we shouldn't fail on aa not being closed
 }
