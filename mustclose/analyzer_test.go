2b84673639c6a6a56b7999e5a944375ed5a7bef1
renzo <renzoarreaza@gmail.com>
2026-06-17 20:20:02 +0200

    move testdata to expected path

--- mustclose/analyzer_test.go
+++ mustclose/analyzer_test.go
@@ -1,19 +1,11 @@
 package mustclose
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func TestAll(t *testing.T) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get wd: %s", err)
-	}
-
-	testdata := filepath.Join(filepath.Dir(wd), "testdata")
-	analysistest.Run(t, testdata, NewAnalyzer())
+	analysistest.Run(t, analysistest.TestData(), NewAnalyzer(), ".")
 }
